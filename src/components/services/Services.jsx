c45f1d39d3ab043e21274e2935c6621f6386ab92
kirat112 <harkiratsingh1128@gmail.com>
2025-02-18 01:33:40 +0530

    cleaned services.jsx

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,16 +1,4 @@
 import { UpdateFollower } from "react-mouse-follower";
-import Icon1 from "../../assets/icons/obj1.png";
-import Icon2 from "../../assets/icons/obj2.png";
-import Icon3 from "../../assets/icons/obj3.png";
-// import TourANC from "../../assets/headphoneInfo/jblTourOneANC.jpeg";
-// import TourMIC from "../../assets/headphoneInfo/jblTourOneMIC.webp";
-// import TourPlayback from "../../assets/headphoneInfo/jblTourOnePlayback.webp";
-// import TuneANC from "../../assets/headphoneInfo/jblTuneANC.webp";
-// import TuneBt from "../../assets/headphoneInfo/jblTuneBt.webp";
-// import TunePlayback from "../../assets/headphoneInfo/jblTunePlayback.webp";
-// import SonySound from "../../assets/headphoneInfo/sonySound.webp";
-// import SonyPair from "../../assets/headphoneInfo/sonyPair.webp";
-// import SonyPlayback from "../../assets/headphoneInfo/sonyPlayback.webp";
 import { motion } from "framer-motion";
 
 export const fadeUp = (delay) => {
@@ -29,106 +17,6 @@ export const fadeUp = (delay) => {
     },
   };
 };
-
-const servicesData = [
-  {
-    id: 1,
-    title: "Security",
-    link: "#",
-    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam nulla possimus repellendus sed fugit doloribus sunt aspernatur perspiciatis facere eveniet.",
-    icon: Icon1,
-    delay: 0.5,
-  },
-  {
-    id: 2,
-    title: "Guarantee",
-    link: "#",
-    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam nulla possimus repellendus sed fugit doloribus sunt aspernatur perspiciatis facere eveniet.",
-    icon: Icon2,
-    delay: 0.8,
-  },
-  {
-    id: 3,
-    title: "Affordability",
-    link: "#",
-    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam nulla possimus repellendus sed fugit doloribus sunt aspernatur perspiciatis facere eveniet.",
-    icon: Icon3,
-    delay: 1.1,
-  },
-];
-
-// const jblTourOneInfo = [
-//   {
-//     id: 1,
-//     name: "JBL Tour One M2",
-//     image: TourANC,
-//     infoTitle: "True Adaptive Noise Cancelling",
-//     infoDesc: `Enjoy the soundtrack of your life no matter where you are.
-//      True Adaptive Noise Cancelling technology automatically uses 4 noise sensing mics to adjust to your surroundings in real-time, minimizing distractions when you need to focus and optimizing the Tour’s performance for a superior audio experience—all while you adjust ANC mode control with the JBL Headphones app.`,
-//   },
-//   {
-//     id: 2,
-//     name: "JBL Tour One M2",
-//     image: TourMIC,
-//     infoTitle: "4-Mic Superior Calls with Voice Control",
-//     infoDesc: `The 4-mic crystal call algorithm of the JBL Tour One M2 allows you to have better conversations even in loud or windy environments or to get help from your preferred voice assistant. And with VoiceAware, you can choose how much of your own voice to hear by controlling the amount of mic input routed back into your earbuds.`,
-//   },
-//   {
-//     id: 3,
-//     name: "JBL Tour One M2",
-//     image: TourPlayback,
-//     infoTitle: "Up to 50 Hours of Playback",
-//     infoDesc: `Listen without limits. Enjoy up to 50 hours of playback, or up to 30 hours with True Adaptive Noise Cancelling enabled. And when you need a boost, just speed charge for 10 minutes and get an extra 5 hours of battery life.`,
-//   },
-// ];
-
-// const jblTuneInfo = [
-//   {
-//     id: 1,
-//     name: "JBL Tune 770NC",
-//     image: TuneANC,
-//     infoTitle: "Adaptive Noise Cancelling with Smart Ambient",
-//     infoDesc: `Adaptive Noise Cancelling means zero distractions when it's time to focus on your studies—or get your groove on. And if you want to hear the world around you without removing your headphones, Ambient Aware and TalkThru sharpen the sounds of your surroundings or voices. Easily activate these Ambient Sound Control modes through the JBL Headphones app.`,
-//   },
-//   {
-//     id: 2,
-//     name: "JBL Tune 770NC",
-//     image: TuneBt,
-//     infoTitle: "Bluetooth 5.3 with LE Audio *",
-//     infoDesc: `Wirelessly stream high-quality JBL Pure Bass Sound from your smartphone with the help of the latest Bluetooth technology. Optimize the Bluetooth performance with the Smart Audio & Video feature in the JBL Headphones app by selecting the Audio (for the best sound quality) or the Video (for an optimal gaming and video output) modes. * Available via OTA update at a later stage.`,
-//   },
-//   {
-//     id: 3,
-//     name: "JBL Tune 770NC",
-//     image: TunePlayback,
-//     infoTitle: "Up to 70H* battery life with speed charge",
-//     infoDesc: `For long-lasting fun, listen wirelessly for up to 44 hours and recharge the battery in as little as 2 hours with the convenient Type-C USB cable. A quick 5-minute recharge gives you 3 additional hours of music. *Battery life: BT on / ANC off`,
-//   },
-// ];
-
-// const sonyInfo = [
-//   {
-//     id: 1,
-//     name: "Sony WH-CH520",
-//     image: SonySound,
-//     infoTitle: "360 Spatial Sound Personalizer",
-//     infoDesc: `With BRAVIA XR™2 and Wireless Transmitter WLA-NS73. Enjoy a thrilling Dolby Atmos® experience and 360 Spatial Sound that adapts to your ears with BRAVIA XR™ and these headphones using wireless transmitter WLA-NS7.4`,
-//   },
-//   {
-//     id: 2,
-//     name: "Sony WH-CH520",
-//     image: SonyPair,
-//     infoTitle: "Multipoint connection",
-//     infoDesc: `For total convenience, these Bluetooth® headphones can be paired with two Bluetooth® devices at the same time. So when a call comes in, your headphones know which device is ringing and connects to the right one automatically.1`,
-//   },
-//   {
-//     id: 3,
-//     name: "Sony WH-CH520",
-//     image: SonyPlayback,
-//     infoTitle: "Stay powered-up for days",
-//     infoDesc: `With up to 50 hours of battery life, you can listen to your favourite music without worrying about running out of charge. And, if your headphones battery is running low, a 3-minute quick-charge can give you 1.5 hours of listening time.`,
-//   },
-// ];
 
 const Services = ({ activeHeadphone }) => {
   return (
@@ -155,7 +43,11 @@ const Services = ({ activeHeadphone }) => {
                   // mixBlendMode: "darken",
                   backgroundElement: (
                     <div>
-                      <img src={data.image} alt="" style={{objectFit:"cover"}}/>
+                      <img
+                        src={data.image}
+                        alt=""
+                        style={{ objectFit: "cover" }}
+                      />
                     </div>
                   ),
                 }}
